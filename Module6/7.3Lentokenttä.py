e1d262a7cba0fdbed4caaef71057404d7f487e45
casper <casperaronen@gmail.com>
2025-09-02 10:24:04 +0300

    Koulu pärinät

--- Module6/7.3Lentokenttä.py
+++ Module6/7.3Lentokenttä.py
@@ -1,4 +1,25 @@
-Lentokemmttä = []
-ICAOkoodi = []
+Lentokentta = ["Helsinki"]
+ICAOkoodi = ["09001"]
 
+def Valinta():
+    valitse = input("Haluatko lisätä tai hakea lentokenttää vaikko lopettaa(uusi, valinta, lopeta): ").lower()
+    return valitse
 while True:
+    toiminto = Valinta()
+    if toiminto == "uusi":
+        lentokentta = input("Minkä nimisen lentokentän haluat lisätä: ")
+        Lentokentta.append(lentokentta)
+        koodi = input("Anna ICAO-koodi ")
+        ICAOkoodi.append(koodi)
+    elif toiminto == "valinta":
+        haku = input("Hae lentokenttä ICAO-koodilla: ")
+        if haku in ICAOkoodi:
+            index = ICAOkoodi.index(haku)
+            print(f"Löytyi: {Lentokentta[index]} (ICAO: {ICAOkoodi[index]})")
+        else:
+            print(f"Lentokenttää ei löytynyt")
+    elif toiminto == "lopeta":
+        print("Ohjelma loppuu babaiiii")
+        break
+    else:
+        print("Virheellinen inputti (forgor mikä input suomeksi oli)")
--- Tuntitehtävät/Peli.py
+++ Tuntitehtävät/Peli.py
@@ -1,13 +1,32 @@
 hahmot = ["Seppo", "Jarmo", "Jaakko"]
+ajoneuvot = ["Bussi", "Auto", "Pyörä"]
 print("Tervetuloa peliini")
 while True:
     try:
-        for i in range(0, len(hahmot)):
-            print(hahmot[i])
+        for i in hahmot:
+            print(i)
         hnPut = input("Valitse hahmosi: ")
         if hnPut in hahmot:
             print("Valitsit", hnPut)
-            print("Pelimme sankari", hnPut, "")
+            print("Pelimme sankari", hnPut, "päätti eräänä kauniina perjantai")
+            print("Käymään hakemaan alkosta vähäsen jano juomaa")
+            print("Muttei hän keksinyt millä mennä hakemaan alkosta juotavaa")
+            print("Millä hänen pitäisi mennä alkoon vai pitäisikö jäädä himaan")
+            for a in ajoneuvot:
+                print(a)
+            aInput = input("Valitse ajoneuvosi")
+            if aInput in ajoneuvot == "Bussi":
+                print("Valitsit", aInput)
+                print("kaupungin sankari", hnPut, "valitsi ajoneuvokseen", aInput)
+                break
+            elif aInput in ajoneuvot == "Auto":
+                print("Valitsit", aInput)
+                print("kaupungin saknari", hnPut, "valitsi ajoneuvokseen", aInput)
+                break
+            elif aInput in ajoneuvot == "Pyörä":
+                print("Valitsit", aInput)
+                print("Kaupungin sankari", hnPut, "valitsi ajoneuvokseen", aInput)
+                break
         else:
             print("Osaatko kirjoittaa")
             print("Tuosta hyvästä peli loppuu")
